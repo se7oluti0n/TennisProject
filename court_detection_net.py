3b07bb233bb405ef0ae577dd03a2180efda0ed09
Truong Tuan Manh <manhattan.vn@gmail.com>
2024-12-20 17:27:25 +0700

    fix bug scale for court detection

--- court_detection_net.py
+++ court_detection_net.py
@@ -12,19 +12,23 @@ class CourtDetectorNet():
         self.model = BallTrackerNet(out_channels=15)
         self.device = device
         if path_model:
-            self.model.load_state_dict(torch.load(path_model, map_location=device))
+            self.model.load_state_dict(torch.load(path_model, map_location=device, weights_only=True))
             self.model = self.model.to(device)
             self.model.eval()
             
     def infer_model(self, frames):
         output_width = 640
         output_height = 360
-        scale = 2
+        scale = []
         
         kps_res = []
         matrixes_res = []
+        print("Ball detector processing")
         for num_frame, image in enumerate(tqdm(frames)):
             img = cv2.resize(image, (output_width, output_height))
+            if not scale:
+                scale.append(image.shape[0]  * 1.0 / output_height)
+                scale.append(image.shape[1]  * 1.0 / output_width)
             inp = (img.astype(np.float32) / 255.)
             inp = torch.tensor(np.rollaxis(inp, 2, 0))
             inp = inp.unsqueeze(0)
@@ -39,8 +43,8 @@ class CourtDetectorNet():
                 circles = cv2.HoughCircles(heatmap, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=2,
                                            minRadius=10, maxRadius=25)
                 if circles is not None:
-                    x_pred = circles[0][0][0]*scale
-                    y_pred = circles[0][0][1]*scale
+                    x_pred = circles[0][0][0]*scale[1]
+                    y_pred = circles[0][0][1]*scale[0]
                     if kps_num not in [8, 12, 9]:
                         x_pred, y_pred = refine_kps(image, int(y_pred), int(x_pred), crop_size=40)
                     points.append((x_pred, y_pred))                
